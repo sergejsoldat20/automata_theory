800b8f5b5707e16d90af001e6753c24d0cc2b98c
Sergej Soldat <sergej.s.soldat20@gmail.com>
2023-05-08 02:24:31 +0200

    equivalent classes

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 from models.dfa import DFA
-
+import itertools
 
 start_state = 'q0'
 alphabet = {'a', 'b'}
 transition_function = {
-    'q0': {'a': 'q5', 'b': 'q1'},
-    'q1': {'a': 'q2', 'b': 'q0'},
-    'q2': {'a': 'q4', 'b': 'q7'},
-    'q3': {'a': 'q6', 'b': 'q1'},
-    'q4': {'a': 'q2', 'b': 'q7'},
-    'q5': {'a': 'q0', 'b': 'q5'},
-    'q6': {'a': 'q7', 'b': 'q5'},
-    'q7': {'a': 'q7', 'b': 'q7'},
+    'q0': {'a': 'q6', 'b': 'q1'},
+    'q1': {'a': 'q2', 'b': 'q4'},
+    'q2': {'a': 'q5', 'b': 'q3'},
+    'q3': {'a': 'q8', 'b': 'q3'},
+    'q4': {'a': 'q6', 'b': 'q8'},
+    'q5': {'a': 'q2', 'b': 'q3'},
+    'q6': {'a': 'q4', 'b': 'q7'},
+    'q7': {'a': 'q8', 'b': 'q7'},
+    'q8': {'a': 'q7', 'b': 'q4'},
+    'q9': {'a': 'q10', 'b': 'q8'},
+    'q10': {'a': 'q3', 'b': 'q9'},
 }
-final_states = {'q1', 'q2', 'q6'}
-states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7'}
+final_states = {'q1', 'q4', 'q8'}
+states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9','q10'}
 automata = DFA(start_state, final_states, transition_function, alphabet, states)
 
 # print(automata.accept('aaba'))
 
-print(automata.get_unreachable_state())
+# print(automata.get_unreachable_state())
 
+# print(len(automata.generate_state_pairs()))
+
+# print(automata.generate_state_pairs() - automata.generate_state_final_pairs())
+
+# print(automata.get_equivalent_states())
+
+# equivalent_pairs = automata.get_equivalent_states()
+
+# print(automata.get_equivalent_classes(equivalent_pairs))
+
+start_state = 'q0'
+alphabet = {'a', 'b'}
 transition_function = {
-    'q0': {'a': 'q1', 'b': 'q0'},
-    'q1': {'a': 'q0', 'b': 'q1'},
-    'q2': {'a': 'q0', 'b': 'q1'}
+    'q0': {'a': 'q4', 'b': 'q1'},
+    'q1': {'a': 'q3', 'b': 'q2'},
+    'q2': {'a': 'q3', 'b': 'q2'},
+    'q3': {'a': 'q2', 'b': 'q3'},
+    'q4': {'a': 'q0', 'b': 'q5'},
+    'q5': {'a': 'q2', 'b': 'q3'},
 }
+final_states = {'q5'}
+states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5'}
+automata = DFA(start_state, final_states, transition_function, alphabet, states)
 
-final_states = {'q1'}
-states = {'q0', 'q1', 'q2'}
+equivalent_pairs = automata.get_equivalent_states()
 
-new_automata = DFA(start_state, final_states, transition_function, alphabet, states)
-print(new_automata.get_unreachable_state())
+print(automata.get_equivalent_classes(equivalent_pairs))
--- models/dfa.py
+++ models/dfa.py
@@ -15,19 +15,20 @@ class DFA:
         # if current_state is in the final_states in the end return true
         return current_state in self.final_states         
             
-
+    # this method finds next states for every character in alphabet 
     def get_next_states(self, state):
         next_states = set()
         for character in self.alphabet:
             next_states.add(self.transitions[state][character])
         return next_states
 
-
+    # this method finds unreachable states
     def get_unreachable_state(self):
-        # we add start state to visible states
+        # we add start state to visited_states
         visited_states = set() 
         visited_states.add(self.start_state)
         reachable_states_size = 0
+        # we go through states and find reachable states 
         while(reachable_states_size < len(visited_states)):
             new_states = set()
             reachable_states_size = len(visited_states)
@@ -35,6 +36,81 @@ class DFA:
                 new_states.update(self.get_next_states(state))
             visited_states.update(new_states)
         return self.states - visited_states
+    
+    def generate_state_final_pairs(self):
+        valid_pairs = set()
+        for state1 in self.states:
+            for state2 in self.states:
+                if state1 in self.final_states and state2 not in self.final_states:
+                    if state1 > state2:
+                        valid_pairs.add((state1,state2))
+                    else:
+                        valid_pairs.add((state2, state1))
+        return valid_pairs
+    
+    def generate_state_pairs(self):
+        all_pairs = set()
+        for state1 in self.states:
+            for state2 in self.states:
+                if state1 != state2:
+                    if state1 > state2:
+                        all_pairs.add((state1,state2))
+                    else:
+                        all_pairs.add((state2, state1))
+        return all_pairs
+        
+    # this method finds next pairs for some pair of states that will be used for minimization algorithm 
+    def find_next_state_pair(self, state_pair):
+        result_pairs = set()
+        for character in self.alphabet:
+            state1 = self.transitions[state_pair[0]][character]
+            state2 = self.transitions[state_pair[1]][character]
+            
+            if state1 > state2:
+                result_pairs.add((state1, state2))
+            else: 
+                result_pairs.add((state2, state1))
+        return result_pairs
+    
+    # this method finds unmarked pairs 
+    def get_equivalent_states(self):
+        state_pairs = self.generate_state_pairs() - self.generate_state_final_pairs()
+        final_pairs = self.generate_state_final_pairs()
 
+        final_pairs_length = 0
 
+        while(final_pairs_length < len(final_pairs)):
+            final_pairs_length = len(final_pairs)
+            new_pairs = set()
+            for pair in state_pairs:
+                next_state_pairs = self.find_next_state_pair(pair)
+                for next_pair in next_state_pairs:
+                    if next_pair in final_pairs:
+                        new_pairs.add(pair)
 
+            state_pairs = state_pairs - new_pairs
+            final_pairs.update(new_pairs)
+
+        return state_pairs
+                        
+    def get_equivalent_classes(self, unmarked_pairs):
+        print(unmarked_pairs)
+        equivalent_classes = list()
+        equivalent_classes_changed = False
+
+        for pair in unmarked_pairs:
+            if len(equivalent_classes) == 0:
+                equivalent_classes.append(pair)
+            else: 
+                for i in range(len(equivalent_classes)):
+                    if set(equivalent_classes[i]).intersection(set(pair)):
+                        equivalent_classes[i] = tuple(set(equivalent_classes[i] + pair)) 
+                        equivalent_classes_changed = True
+                if equivalent_classes_changed:
+                    equivalent_classes_changed = False
+                else:
+                    equivalent_classes.append(pair)
+        # return set(equivalent_states)
+        return equivalent_classes
+
+                     
